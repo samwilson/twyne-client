f20691460e46e2d7e515de3c85a39ce76e75c914
Sam Wilson <sam@samwilson.id.au>
2021-04-19 14:32:02 +0800

    Add config helper and storage

--- src/Command/UploadCommand.php
+++ src/Command/UploadCommand.php
@@ -9,28 +9,26 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\Mime\Part\DataPart;
 use Symfony\Component\Mime\Part\Multipart\FormDataPart;
 use Symfony\Contracts\HttpClient\ResponseInterface;
 
-class UploadCommand extends Command
+class UploadCommand extends CommandBase
 {
 
-    /** @var InputInterface */
-    private $input;
-
-    /** @var SymfonyStyle */
-    private $output;
+    /** @var string */
+    private $url;
 
     protected function configure()
     {
+        $config = $this->getConfig();
+        $apiKeyDefault = !empty($config['api_key']) ? '[Stored in config]' : '';
         $this->setName('upload')
             ->setDescription('Upload files.')
             ->addArgument('source', InputArgument::REQUIRED, 'A directory or file name.')
-            ->addOption('url', 'u', InputOption::VALUE_REQUIRED, 'The Twyne site base URL.')
-            ->addOption('apikey', 'k', InputOption::VALUE_REQUIRED, 'The Twyne API key.')
+            ->addOption('url', 'u', InputOption::VALUE_REQUIRED, 'The Twyne site base URL.', $config['url'])
+            ->addOption('apikey', 'k', InputOption::VALUE_REQUIRED, 'The Twyne API key.', $apiKeyDefault)
             ->addOption('tags', 't', InputOption::VALUE_REQUIRED, 'Semicolon-separated tags.', '')
             ->addOption(
                 'timezone',
@@ -39,23 +37,38 @@ class UploadCommand extends Command
                 'The timezone of the times in the EXIF data of any uploaded photos.',
                 'Z'
             )
-            ->addOption('author', 'a', InputOption::VALUE_REQUIRED, 'Author name.', 'Anon.')
-            ->addOption('group', 'g', InputOption::VALUE_REQUIRED, 'The name of the user group.', 'Private');
+            ->addOption('author', 'a', InputOption::VALUE_REQUIRED, 'Author name.', $config['default_author'])
+            ->addOption(
+                'group',
+                'g',
+                InputOption::VALUE_REQUIRED,
+                'The name of the user group.',
+                $config['default_group']
+            );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->input = $input;
-        $this->output = new SymfonyStyle($input, $output);
+        parent::execute($input, $output);
 
+        // URL.
+        $urlOpt = $this->input->getOption('url');
+        if (!$urlOpt) {
+            $this->output->error('Required option: url');
+            return Command::FAILURE;
+        }
+        $this->url = rtrim($urlOpt, '/');
+        $this->output->writeln('Uploading to ' . $this->url);
+
+        // Source directory or file.
         $sourceArg = $input->getArgument('source');
         $source = realpath($sourceArg);
-
         if (!$source) {
             $this->output->error('Invalid source: ' . $sourceArg);
             return Command::FAILURE;
         }
 
+        // Upload.
         if (is_dir($source)) {
             $this->uploadDirectory($source);
         } else {
@@ -86,11 +99,6 @@ class UploadCommand extends Command
 
     private function uploadOne(string $filename): void
     {
-        $urlOpt = $this->input->getOption('url');
-        if (!$urlOpt) {
-            throw new Exception('Required option: url');
-        }
-        $url = rtrim($urlOpt, '/');
 
         $authorName = $this->input->getOption('author');
         if (!$authorName) {
@@ -98,7 +106,11 @@ class UploadCommand extends Command
         }
 
         $client = HttpClient::create();
-        $authHeader = 'Twyne api_key=' . $this->input->getOption('apikey');
+        $apiKey = $this->input->getOption('apikey');
+        if (!empty($apiKey)) {
+            $apiKey = $this->getConfig()['api_key'];
+        }
+        $authHeader = 'Twyne api_key=' . $apiKey;
         // Check by checksum.
         $options = [
             'query' => [
@@ -108,7 +120,7 @@ class UploadCommand extends Command
                 'Authorization' => $authHeader,
             ],
         ];
-        $response1Data = $client->request('GET', $url . '/post/search', $options);
+        $response1Data = $client->request('GET', $this->url . '/post/search', $options);
         $response1 = $this->getJson($response1Data);
         if ($response1['post_count'] > 0) {
             $this->output->writeln('Already exists as ' . $response1['posts'][0]['url'] . ' -- ' . $filename);
@@ -129,7 +141,7 @@ class UploadCommand extends Command
             'headers' => array_merge($formData->getPreparedHeaders()->toArray(), ['Authorization' => $authHeader]),
             'body' => $formData->bodyToIterable(),
         ];
-        $response2Data = $client->request('POST', $url . '/upload-api', $options);
+        $response2Data = $client->request('POST', $this->url . '/upload-api', $options);
         $response2 = $this->getJson($response2Data);
         if (isset($response2['upload_count']) && $response2['upload_count'] > 0) {
             foreach ($response2['success'] as $success) {
